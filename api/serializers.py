683e85f79fc1027067cb97caee96dbdd4e1fca57
RezenkovD <rezenkovdmitro@gmail.com>
2022-08-06 09:39:56 +0300

    fixed the file serializers

--- api/serializers.py
+++ api/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from api.models import Member, Project
 
 
-class MemberSerializer(serializers.HyperlinkedModelSerializer):
+class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
         fields = '__all__'
 
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    members = serializers.HyperlinkedIdentityField(view_name="api:members-detail")
+
     class Meta:
         model = Project
         fields = '__all__'
