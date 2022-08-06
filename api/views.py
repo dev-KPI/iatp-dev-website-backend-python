7cd487e08135f4fabd8ebd8296b6a3db60955e8a
RezenkovD <rezenkovdmitro@gmail.com>
2022-08-06 10:14:32 +0300

    create filter in member

--- api/views.py
+++ api/views.py
@@ -3,10 +3,14 @@ from rest_framework import viewsets
 from api.serializers import MemberSerializer, ProjectSerializer, LanguageSerializer
 from api.models import Member, Project, Language
 
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['programming_language', ]
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
