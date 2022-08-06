e1bf16eda9798f7becd93864349d0d5bf090d967
RezenkovD <rezenkovdmitro@gmail.com>
2022-08-06 10:24:59 +0300

    create search filter in member

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 from api.serializers import MemberSerializer, ProjectSerializer, LanguageSerializer
 from api.models import Member, Project, Language
@@ -9,8 +10,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['programming_language', ]
+    search_fields = ['last_name', ]
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
